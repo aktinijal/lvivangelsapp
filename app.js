f1ae168d4b5a85b82d4b7d1e289dc625dc4f3425
Kate Grytsenko <aktinijal@gmail.com>
2016-09-26 23:07:26 +0300

    deleted slick folder

--- app.js
+++ app.js
@@ -30,25 +30,13 @@ app.config(function ($routeProvider) {
     // $locationProvider.html5Mode(true);
 });
 
-// .directive('slickSlider', function($timeout){
-//     return {
-//     restrict: 'A',
-//     link: function(scope,element,attrs) {
-//         $timeout(function() {
-//             $(element).slick(scope.$eval(attrs.slickSlider));
-//             });
-//         }
-//     }
-// }); 
 app.directive("scroll", function ($window) {
     return function(scope, element, attrs) {
         angular.element($window).bind("scroll", function() {
              if (this.pageYOffset >= 100) {
                  scope.boolChangeClass = true;
-                 console.log('Scrolled below header.');
              } else {
                  scope.boolChangeClass = false;
-                 console.log('Header is in view.');
              }
             scope.$apply();
         });
