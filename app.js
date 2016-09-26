7245e517f08dcab7b0750df4668f6c9f62fcd8a8
Dmitriy Bodlev <demo4nek@mail.ru>
2016-09-26 13:48:33 +0300

    slideToggle News

--- app.js
+++ app.js
@@ -54,3 +54,27 @@ app.directive("scroll", function ($window) {
         });
     };
 });
+app.controller('eventsController', function($scope) {
+  $scope.box1 = $scope.box2 = $scope.box3 = true;
+});
+    
+app.directive('slideToggle', function() {  
+  return {
+    restrict: 'A',      
+    scope:{
+      isOpen: "=slideToggle" // 'data-slide-toggle' in our html
+    },  
+    link: function(scope, element, attr) {
+      var slideDuration = parseInt(attr.slideToggleDuration, 10) || 200;      
+      
+      // Watch for when the value bound to isOpen changes
+      // When it changes trigger a slideToggle
+      scope.$watch('isOpen', function(newIsOpenVal, oldIsOpenVal){
+        if(newIsOpenVal !== oldIsOpenVal){ 
+          element.stop().slideToggle(slideDuration);
+        }
+      });
+      
+    }
+  };  
+});
